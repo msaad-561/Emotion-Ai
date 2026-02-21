90387cdd9c658d75e0516e29d02bfb2824639257
msaad-561 <msaadfr561@gmail.com>
2026-02-21 12:18:15 +0500

    Delete test_fer.py

--- test_fer.py
+++
@@ -1,8 +0,0 @@
-try:
-    import fer
-    print(f"FER location: {fer.__file__}")
-    from fer import FER
-    print("Import Successful")
-except Exception as e:
-    import traceback
-    traceback.print_exc()
